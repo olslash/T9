9922f94c888f95dd9ba35b5837aee6148f3eb2ed
Mitch Robb <olordhux+github@gmail.com>
2014-06-09 12:27:18 -0700

    hook up basic keyboard input

--- T9.js
+++ T9.js
@@ -15,6 +15,11 @@ words.forEach(function(word) {
   trie.insert(word);
 });
 
+$('.keyboard').change(function(e){
+  console.log(e.target.value);
+  console.log(trie.getClosestFullWords(e.target.value, 3));
+});
+
 // groups of 3 letters per key
 // if I press 1 (a,b,c):
 // getClosestFullWords(oldword + a)
@@ -22,3 +27,60 @@ words.forEach(function(word) {
 // getClosestFullWords(oldword + c)
 
 //keep a good result from each
+
+// var nodeDia = 50;
+
+// var svg = d3.select('body')
+//   .append('svg')
+//   .attr('width', 100)
+//   .attr('height', 1000);
+
+// var nodes = svg.selectAll('circle')
+//   .data(words)
+//   .enter()
+//   .append('circle');
+
+// nodes.attr({
+//   'cy': function(_, i) {
+//     return (i * 50) + 25;
+//   },
+//   cx: nodeDia / 2,
+//   r: function(d) {
+//     return d.length * 2;
+//   },
+//   fill: 'lightgrey',
+//   stroke: 'orange',
+//   'stroke-width': 2
+// });
+
+// var lines = svg.selectAll('line')
+//   .data(words)
+//   .enter()
+//   .append('line');
+// lines.attr({
+//   x1:,
+//   x2: ,
+//   y1: ,
+//   y2: 
+// });
+
+// svg.selectAll('text')
+//   .data(words)
+//   .enter()
+//   .append('text')
+//   .text(function(d) {
+//     return d;
+//   })
+//   .attr({
+//     x: 50,
+//     y: function(_, i) {
+//       return (i * 50) + 25;
+//     }
+//   });
+// .attr('cy', nodeDia / 2)
+// .attr('r', function(d) {
+//   return d.length * 2;
+// })
+// .attr('fill', 'lightgrey')
+// .attr('stroke', 'orange')
+// .attr('stroke-width', 2);
